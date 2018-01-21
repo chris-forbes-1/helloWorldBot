16327b2faab07a08c3f4f640f6e741ee3d0e5570
root <christopherforbes1@googlemail.com>
2018-01-21 00:08:25 +0000

    Removed ludicrous express api to a discord bot..

--- index.js
+++ index.js
@@ -1,57 +1,9 @@
 const Discord = require('discord.js');
-const express = require('express');
-const app = new express();
-const port = 8080;
 const bodyParser     = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port,()=>{
     const client = new Discord.Client();
-    console.log('listening on port ' + port);
-    app.get('/',(req,res)=>{
-        res.send('hello ' + client.user );
-    });
-    
-    app.post('/message', (req, res) =>{
-        console.log('message received');
-        console.log(req.body);
-        let guildId = req.body['guildid'];
-        let channelid = req.body['channelid'];
-        let message = req.body['message'];
-        if(guildId === undefined || guildId === null || guildId === '') {
-            res.send('nope');
-            return;
-        } 
-        if(channelid === undefined || channelid === null || channelid === '') {
-            res.send('no');
-            return;
-        }
-        if(message === undefined || message === null || message === '') {
-            res.send('nada');
-            return;
-        }
-        let guild = client.guilds.get(guildId);
-        if (guild === undefined || guild == null)
-        {
-            res.send('no idea what youre talking about');
-            return;
-        }
-        else 
-        {
-            let channel = guild.channels.get(channelid);
-            if(channel === undefined || channel === null) 
-            {
-                res.send('no idea what youre talking about');
-                return;
-            }
-            else
-            {
-                channel.send(message);
-            }
-        }
 
-        res.send('ok');
-    });
+    
     /*
     * Static function declarations
     */
@@ -59,14 +11,14 @@ app.listen(port,()=>{
         let contents = getMessageContent(msg);
         let guild = client.guilds.get(contents[1]);
         if(guild === null || guild === '' || guild === undefined) {
-            msg.channel.send('no guild of id: ' + contents[1] + ' found');
+            msg.channel.send(':arrow_right:no guild of id: ' + contents[1] + ' found');
         } 
         else  {
             let channel = guild.channels.get(contents[2]);
             if (channel === null || channel === '' || channel === undefined) {
                 msg.channel.send("Could not find channel : " + contents[2] + " on guild " + contents[1]);
             } else {
-                channel.send('this is a test of message from the client of this channel!');
+                channel.send(`:arrow_right: ${contents[3]}`);
             }
         }
     }
@@ -75,62 +27,182 @@ app.listen(port,()=>{
     };
     
     const adminMessageReceived = (msg) =>{
-        
-            if(msg.content === 'ping'){
-                msg.channel.send(client.ping  + ' ms');
-            }
-            if(msg.content === 'connected_guild_debug') {
-                msg.channel.send('Member of guilds:');
+            if(msg.content === '.debug_guild_permissions') {
+                msg.channel.send(':arrow_right:Member of guilds:');
                client.guilds.forEach((k,v) => {           
                    if (v != '') {
-                        msg.channel.send(v + ' -> ' + k.name);
+                        msg.channel.send(":arrow_right:"+ v + ' -> ' + k.name);
                    }
                });
+               return true;
             }
-            if(msg.content.startsWith('guild_debug'))
+            
+            if(msg.content.startsWith('.admin_announce'))
+            {
+                sendMessageToChannel((msg));
+            }
+
+            if(msg.content.startsWith('.debug_guild'))
             {
                 let contents = getMessageContent(msg);
                 let guild = client.guilds.get(contents[1]);
                 if(guild === null || guild === '' || guild === undefined) { 
-                    msg.channel.send('No guild of id: ' + contents[1] + ' found');
+                    msg.channel.send(':arrow_right:No guild of id: ' + contents[1] + ' found');
                 } else {
-                    msg.channel.send('Channels available in guild: ' + guild.name);
+                    msg.channel.send(':arrow_right:Channels available in guild: ' + guild.name);
                     guild.channels.forEach((k,v)=>{
                         if(v != '') {
-                            msg.channel.send(v + " -> " + k.name);
+                            msg.channel.send(":arrow_right: " + v + " -> " + k.name);
                         }
                     });
                 }
+                return true;
             }
-            if(msg.content.startsWith('permissions')) {
+
+            if(msg.content.startsWith('.debug_permissions')) {
                 let contents = getMessageContent(msg);
                 let guildMember = msg.member;
                 if(guildMember === undefined) {
-                    msg.channel.send('Unknown guild member:' + msg.author);
+                    msg.channel.send(':arrow_right:Unknown guild member:' + msg.author);
                 } else {
-                    let member = guildMember.permissionsFor(guildMember);
-                    console.log(msg);
+                    msg.channel.send(':arrow_right:permissions bit field: ' + guildMember.permissions.bitfield);
                 }
+                return true;
             }
+
+            if(msg.content.startsWith('.status')) {
+                let healthStatus = 'Client Status:' +  (client.status === 0 ? ':ok:'  : ':thumbsdown::skin-tone-4:')  + '\n'
+                +':up: Uptime: ' + client.uptime +'\n' 
+                + ':ping_pong: Current ping: ' + client.ping + 
+                '\n :ideograph_advantage:client tag: ' + client.user.tag;
+
+                msg.channel.send(healthStatus);
+                return true;
+            }
+
+            if(msg.content.startsWith(".refresh_connection_status")){
+                msg.channel.send("Ok reloading session context");
+                client.destroy().then(()=>{
+                    client.login(process.env.DiscordAFKBotApiKey);
+                });
+                return true;
+            }
+            return false;
         }
+        const getUsersAvatar = (msg, user) => {
+            let membersOfMessageGuild = msg.channel.guild.members;
+            let found = false;
+            membersOfMessageGuild.forEach(function(item, key, map){
+                let userObj = item.user;
+
+                if(item.nickname ? item.nickname.toLowerCase() === user : false || userObj.username.toLowerCase() === user)
+                {
+                    found = true;
+                    msg.channel.send('the avatar for user ' + user + " is: " + userObj.avatarURL);
+                }
+            });
+            if(!found){
+                msg.channel.send(`no user ${user} found`);
+            }
+        };
+        const userInfo = (msg, user) =>{
+            let membersOfMessageGuild = msg.channel.guild.members;
+            let found = false;
+            membersOfMessageGuild.forEach(function(item, key, map){
+                let userObj = item.user;
+
+                if(item.nickname ? item.nickname.toLowerCase() === user : false || userObj.username.toLowerCase() === user)
+                {
+                    found = true;
+                    let response = `:id: ${userObj.id}\n:globe_with_meridians:AvatarUrl:<${userObj.avatarURL}}>\n:astonished:Last Message:${userObj.lastMessage}\n`;
+                    response += `:alarm_clock:Created at${userObj.createdAt}`;
+                    let usersRoles = "";
+                    item.roles.forEach(function(a,b,c){
+                        usersRoles += a.name +",";
+                    });
+                    response += `\n:trolleybus: user roles: ${usersRoles}`;
+                    msg.channel.send(response);
+                }
+            });
+            if(!found){
+                msg.channel.send(`no user found for ${user}`);
+            }
+        };
+        const doNameBasedCall = (msg, func) => {
+            let contents = getMessageContent(msg);
+            if(contents.length >1)
+            {
+                let targetUser = '';
+                for(var i = 1; i < contents.length; i++)
+                {
+                    targetUser = targetUser +" "+contents[i];
+                }
+                func(msg, targetUser.trim().toLowerCase());
+            }
+            else
+            {
+                msg.channel.send("command format is .<command> <users name>");
+            }
+        };
+
+        const isMortalCommand = (msg) => {
+            if(msg.mentions.everyone) {
+                msg.channel.send('',{file:'http://i0.kym-cdn.com/photos/images/newsfeed/001/291/784/c32.jpg'});
+            }
+            else
+            {
+                let message_received = msg.content;
+                if(message_received.startsWith("."))
+                {
+                    if(message_received.startsWith(".avatar"))
+                    {
+                        doNameBasedCall(msg, getUsersAvatar);
+                    }
+                    else if(message_received.startsWith(".userInfo"))
+                    {
+                        doNameBasedCall(msg, userInfo);
+                    }
+                }
+                
+            }
+        };
 
 
     client.on('ready',()=>{
         console.log('Logged in');
-        
     });
     
     
     
     client.on('message', msg=>{
-        if(msg.author.id === '169166156043649035')
-        {
-         adminMessageReceived(msg);   
-        }   
+        try {
+            if(msg.author.id === '169166156043649035')
+            {
+                if(!adminMessageReceived(msg))
+                {
+                    isMortalCommand(msg);
+                }
+            }   
+            else
+            {
+                if(msg.author.id != '357260268574867466')
+                {
+                    isMortalCommand(msg);
+                }
+                else
+                {
+                    console.log("Filtered my own message");
+                }
+            }
+        } catch (error) {
+            msg.channel.send('command failed');
+            msg.channel.send(error.message);
+        }
     });
     client.login(process.env.DiscordAFKBotApiKey);
-    
-});
 
-
-
+    client.on('ready',()=>{
+        console.log("setting game value");
+        client.user.setPresence({game:{name:'in debug mode'}});
+        client.user.setUsername("defenestrate bot");
+    });
